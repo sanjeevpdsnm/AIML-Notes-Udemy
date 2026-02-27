3e490b97870f86bc16b2e527951331147679bc87
sanjeevpdsnm <sanjeev2005p.05@gmail.com>
2026-02-27 19:40:46 +0530

    AIML COURSES

---
+++  Section 15/logs/logger.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(
+                    filename="app.log",
+                    filemode="w",
+                    level=logging.DEBUG,
+                    format="%(asctime)s - %(levelname)s - %(message)s",
+                    datefmt="%Y-%m-%d %H:%M:%S"                    
+)
---
+++  Section 8/packagessolution/my_pakage/__init__.py
@@ -0,0 +1,3 @@
+# from my_pakage import module1,module2
+from .module1 import add
+from .module2 import mult
